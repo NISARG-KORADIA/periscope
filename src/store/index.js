6ea530c051700e6e30d49ae0a604e074ed7a4efc
NISARG-KORADIA <mknisarg@gmail.com>
2022-11-20 14:26:40 +0530

    Messages now work on API

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import auth from './authSlice';
 import activate from './activateSlice';
 import roomModal from './roomModalSlice';
 
-// here we are passing the both reducer to the store.
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+}
+
+const reducer = combineReducers({
+  auth,
+  activate,
+  roomModal,
+});
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    auth,
-    activate,
-    roomModal,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
